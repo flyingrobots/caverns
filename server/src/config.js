39d41a4d6b08b14014f9fae1860a569455f3c637
James Ross <jkross@gmail.com>
2013-08-31 21:03:38 -0700

    [server] Adds sandbox mode

--- server/src/config.js
+++ server/src/config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 
+// TODO make this config-driven
 var clientPath = path.resolve(__dirname, '../../client');
 
 exports.getClientPath = function() {
@@ -8,4 +9,14 @@ exports.getClientPath = function() {
 
 exports.getContentPath = function() {
   return clientPath + "/content";
+}
+
+// TODO kill once everything is config-driven
+exports.getSandboxPath = function() {
+  return path.resolve(__dirname, '../../sandbox');
+}
+
+// TODO kill once everything is config-driven
+exports.getSandboxContentPath = function() {
+  return path.resolve(__dirname, '../../sandbox/content');
 }
