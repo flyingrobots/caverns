bbf7c484a546c3f1ed3f9bacb12b8fe2722ac57d
James Ross <jkross@gmail.com>
2013-09-02 12:09:20 -0700

    [sandbox][physics demo] Random cool stuff.

--- sandbox/src/physics/demo.js
+++ sandbox/src/physics/demo.js
@@ -9,12 +9,33 @@ var start = function(game, width, height) {
   game.graphics = new Graphics(game);
 }
 
+//---------------------------------------------------------------------------
 var colors = {
   "gray": 0x2d2d2d,
   "orange": 0xf99157,
   "blood": 0xc96161,
-  "blue": 0x6199cc
+  "blue": 0x6199cc,
+  "green": 0x72B897,
+  "yellow": 0xEEFF66,
+  "purple": 0xCC99C2
 };
+
+//---------------------------------------------------------------------------
+var randomInRange = function(min, max) {
+  return Math.round(min + Math.random() * (max - min));
+}
+
+//---------------------------------------------------------------------------
+var randomColor = function() {
+  var colorsArray = [
+    colors.blood,
+    colors.blue,
+    colors.green,
+    colors.yellow,
+    colors.purple
+  ];
+  return colorsArray[randomInRange(0, colorsArray.length - 1)];
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 var PhysicsDemo = new Class({ Extends: Game,
@@ -26,10 +47,31 @@ var PhysicsDemo = new Class({ Extends: Game,
   },
   //---------------------------------------------------------------------------
   setup: function() {
-    this.objectPool.addBoxObject(100, 250, 100, 5, colors.orange, true);
-    this.objectPool.addBoxObject(100, 0, 10, 10, colors.blue, false);
-    this.objectPool.addBoxObject(100, 20, 10, 10, colors.blood, false);
-    this.objectPool.addBoxObject(100, 40, 10, 10, colors.orange, false);
+    // some static dudes
+    this.objectPool.addBoxObject(200, 250, 200, 5, colors.orange, true);
+    var staticCount = randomInRange(1, 5);
+    while (staticCount-- > 0) {
+      var staticBox = this.objectPool.addBoxObject(
+        randomInRange(0, 400), 
+        randomInRange(100, 180),
+        randomInRange(3, 40),
+        randomInRange(3, 5),
+        colors.orange, 
+        true
+      );
+    }
+    // some dynamic dudes
+    var boxCount = randomInRange(3, 8);
+    while (boxCount-- > 0) {
+      this.objectPool.addBoxObject(
+        randomInRange(0, 300), 
+        randomInRange(0, 40), 
+        randomInRange(5, 30), 
+        randomInRange(5, 30),
+        randomColor(), 
+        false
+      );
+    }
   },
   //---------------------------------------------------------------------------
   render: function() {
